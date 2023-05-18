cacceb3f7f21aa670bbe95bc72e6c6bc328651b2
Winston <44872771+winston-yallow@users.noreply.github.com>
2023-05-18 16:56:52 +0200

    Use right click to rotate rulers (closes #22)

--- project/src/dynamic_map/draggable_control.gd
+++ project/src/dynamic_map/draggable_control.gd
@@ -2,20 +2,19 @@ class_name DraggableControl
 extends Control
 
 
-var active := false
+enum STATE { NONE, DRAGGING, ROTATING }
 
-var dragging := false
+var active := false
+var current_state := STATE.NONE
+
 var drag_object_start := Vector2.ZERO
 var drag_mouse_start := Vector2.ZERO
 
-var rotating := false
 var rotation_anchor := Vector2(0, size.y)
 
 
 func activate() -> void:
 	active = true
-	dragging = false
-	rotating = false
 
 
 func deactivate() -> void:
@@ -26,42 +25,43 @@ func _gui_input(event: InputEvent) -> void:
 	if not active:
 		return
 	
-	if not dragging:
-		_handle_waiting(event)
-	else:
-		_handle_dragging(event)
+	match current_state:
+		STATE.NONE: _handle_waiting(event)
+		STATE.DRAGGING: _handle_dragging(event)
+		STATE.ROTATING: _handle_rotating(event)
 
 
 func _handle_waiting(event: InputEvent) -> void:
 	if event.is_action_pressed("map_interact_primary"):
-		dragging = true
-		rotating = false
+		current_state = STATE.DRAGGING
 		drag_object_start = position
 		drag_mouse_start = event.global_position
+		get_tree().root.set_input_as_handled()
+	elif event.is_action_pressed("map_interact_secondary"):
+		current_state = STATE.ROTATING
 		get_tree().root.set_input_as_handled()
 
 
 func _handle_dragging(event: InputEvent) -> void:
 	if event.is_action_released("map_interact_primary"):
-		dragging = false
+		current_state = STATE.NONE
 		get_tree().root.set_input_as_handled()
 	
 	elif event is InputEventMouseMotion:
-		
-		if event.shift_pressed and not rotating:
-			rotating = true
-		elif not event.shift_pressed and rotating:
-			rotating = false
-			drag_object_start = position
-			drag_mouse_start = event.global_position
-		
-		if rotating:
-			var before := get_global_transform() * rotation_anchor
-			var dir := before.direction_to(event.global_position)
-			rotation = dir.angle()
-			var after := get_global_transform() * rotation_anchor
-			position -= after - before
-		else:
-			var diff: Vector2 = event.global_position - drag_mouse_start
-			position = drag_object_start + diff
-			get_tree().root.set_input_as_handled()
+		var diff: Vector2 = event.global_position - drag_mouse_start
+		position = drag_object_start + diff
+		get_tree().root.set_input_as_handled()
+
+
+func _handle_rotating(event: InputEvent) -> void:
+	if event.is_action_released("map_interact_secondary"):
+		current_state = STATE.NONE
+		get_tree().root.set_input_as_handled()
+	
+	elif event is InputEventMouseMotion:
+		var before := get_global_transform() * rotation_anchor
+		var dir := before.direction_to(event.global_position)
+		rotation = dir.angle()
+		var after := get_global_transform() * rotation_anchor
+		position -= after - before
+		get_tree().root.set_input_as_handled()
