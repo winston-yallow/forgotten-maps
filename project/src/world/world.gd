b02428c07b7dc0ca73881d9e8e93e5df9e3d2c36
Winston <44872771+winston-yallow@users.noreply.github.com>
2023-05-20 18:12:01 +0200

    Populate world using scatter addon

---
+++ project/src/world/world.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+var load_steps_done := 0
+var load_steps_total := 10
+
+
+func _ready() -> void:
+	var nodes := get_tree().get_nodes_in_group("scatter")
+	load_steps_total = nodes.size()
+	for node in nodes:
+		node.build_completed.connect(_complete_load_step)
+
+
+func _complete_load_step() -> void:
+	load_steps_done += 1
+	print(load_steps_done, "/", load_steps_total)
+
